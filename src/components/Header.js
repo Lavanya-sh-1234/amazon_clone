57f470dd206b0d3aa7a313f16e588b8031fb00c8
Lavanya Singh <lavanya.sh7@gmail.com>
2025-05-15 00:19:05 +0530

    feat(auth): add Google Sign-In with NextAuth

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,11 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Header() {
+  const { data: session } = useSession();
+
   return (
     <header>
       {/*Top nav */}
@@ -31,8 +34,8 @@ export default function Header() {
 
         {/*Right*/}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap ">
-          <div className="link">
-            <p> Hello User</p>
+          <div onClick={!session ? signIn : signOut} className="link">
+            <p> {session ? `Hello, ${session.user.name}` : "Sign In"}</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
 
