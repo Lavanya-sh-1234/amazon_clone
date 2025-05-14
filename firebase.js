e4afd759d9e92911d914aaa68ce215ab17eef7fb
Lavanya Singh <lavanya.sh7@gmail.com>
2025-05-15 00:19:05 +0530

    feat(auth): add Google Sign-In with NextAuth

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtTza8FEyShi5daM223CNkAdHfeP54Syg",
+  authDomain: "amzn-2-fa616.firebaseapp.com",
+  projectId: "amzn-2-fa616",
+  storageBucket: "amzn-2-fa616.firebasestorage.app",
+  messagingSenderId: "1042297601437",
+  appId: "1:1042297601437:web:054ee69b3dee743ac44374"
+};
